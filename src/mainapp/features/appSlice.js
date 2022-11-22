ee02d0ce0d1d5c02e4382a7ed9e9d8d8e4466bcf
skht <iamsumankht@gmail.com>
2022-11-22 20:10:02 +0545

    DeploymentL1

--- src/mainapp/features/appSlice.js
+++ src/mainapp/features/appSlice.js
@@ -24,16 +24,12 @@ export const appSlice = createSlice({
     setResult: (state, action) => {
       const { text, userInput, inc } = action.payload;
       let correctWords = 0;
-      console.log(userInput, text);
 
       for (let item of userInput.split(" ")) {
         if (text.split(" ").includes(item)) {
           correctWords++;
         }
       }
-
-      console.log(correctWords);
-      console.log(state.timerValue);
 
       const wpm =
         (correctWords / (state.timerValue === 0 ? 1 : state.timerValue)) * 60;
